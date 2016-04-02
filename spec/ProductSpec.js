308051a4b0edd850d1e34c869f5776e79472af82
Frank Tan <francis.tine@gmail.com>
2016-04-02 13:25:18 +0800

    feat: add a function which takes an array and outputs an array of products of all other elements

---
+++ spec/ProductSpec.js
@@ -0,0 +1,17 @@
+describe('Product', function () {
+  'use strict';
+
+  beforeEach(function () {
+
+  });
+
+  it('Works for simple two element array', function () {
+    var productArray = Product.productOfAllOthers([1, 2]);
+    expect(productArray).toEqual([2, 1]);
+  });
+
+  it('Works for normal array', function () {
+    var productArray = Product.productOfAllOthers([1, 7, 3, 4]);
+    expect(productArray).toEqual([84, 12, 28, 21]);
+  });
+});
---
+++ src/Product.js
@@ -0,0 +1,40 @@
+var Product = (function () {
+
+  // For a given array, eg, [1,2,3], return an array with each element being
+  // the product of all other elements. Eg, [6,3,2]
+  // No division is allowed
+  // Time compexity O(n)
+  // Space complexity O(n)
+  var productOfAllOthers = function (array) {
+    var length, arrayProductOthers, productSofar;
+
+    ArrayValidation.notUndefined(array);
+    ArrayValidation.notNull(array);
+    ArrayValidation.mustBeArrayType(array);
+    ArrayValidation.atLeastTwoElements(array);
+
+    length = array.length;
+    arrayProductOthers = [];
+
+    // calculate an array of product before each element
+    productSofar = 1;
+    for (var i = 0; i < length; i++) {
+      arrayProductOthers[i] = productSofar;
+      productSofar *= array[i];
+    }
+
+    // calculate an array of product after each element 
+    // and multiply to the result array
+    productSofar = 1;
+    for (var j = length - 1; j >= 0; j--) {
+      arrayProductOthers[j] *= productSofar;
+      productSofar *= array[j];
+    }
+
+    return arrayProductOthers;
+  };
+
+  return {
+    productOfAllOthers: productOfAllOthers
+  };
+})();
